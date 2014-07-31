81cd72165310cc749047af8b3e17ccb9f42149c5
Charmander <~@charmander.me>
2014-07-31 09:11:53 -0700

    Clear rate limits on server startup.
    This fixes the issue of the limits never expiring if the server is
    stopped while any have been exceeded.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var app = require('./application');
+var rateLimit = require('./lib/rate-limit');
+
+rateLimit.clear().done();
 
 var server = app.listen(
 	process.env.BIND || 3000,

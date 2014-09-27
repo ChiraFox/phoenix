7c5235fde93c1cd6a6f9a551ba8b4ef4f703e2da
Charmander <~@charmander.me>
2014-09-26 18:25:47 -0700

    Move file handling into form-session.
    This greatly simplifies controllers accepting file uploads.
    Upload limits haven’t been implemented yet.

--- controllers/submissions.js
+++ controllers/submissions.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var Promise = require('bluebird');
 var util = require('util');
 var formSession = require('../lib/form-session');
 var permission = require('../lib/permission');
@@ -16,75 +15,33 @@ var uploadForm = permission.require('submit', view('submissions/upload', [notifi
 var createForm = permission.require('submit', view('submissions/edit', [tags.mostCommonTagsForRequester, media.forRequest, notifications.counts]));
 
 var upload = permission.require('submit', function upload(request) {
-	return formSession.formFiles(request).then(function (form) {
-		var submissionUpload = Promise.resolve(null);
-
-		form.on('file', function processFile(name, file) {
-			if (name !== 'file') {
-				file.stream.resume();
-				return;
-			}
-
-			form.removeListener('file', processFile);
-
-			submissionUpload = media.upload(file.stream);
-		});
-
-		function completeUpload() {
-			return submissionUpload.then(function (submissionMedia) {
-				return submissionMedia ?
-					new Redirect('/submissions/new?submit=' + submissionMedia.hash, Redirect.SEE_OTHER) :
-					new Redirect('/submissions/upload');
-			});
-		}
-
-		return new Promise(function (resolve) {
-			form.on('finish', function () {
-				resolve(completeUpload());
-			});
-		});
+	return formSession.formFiles({ file: {} }, request).then(function (form) {
+		return form.files.file;
+	}).then(function (submissionMedia) {
+		return submissionMedia ?
+			new Redirect('/submissions/new?submit=' + submissionMedia.hash, Redirect.SEE_OTHER) :
+			new Redirect('/submissions/upload');
 	});
 });
 
 var create = permission.require('submit', function create(request) {
-	return formSession.formFiles(request).then(function (form) {
-		var thumbnailUpload = Promise.resolve(null);
-
-		function completeSubmission() {
-			return media.byHash(form.fields.media).then(function (submitMedia) {
-				return thumbnailUpload.then(function (thumbnailMedia) {
-					return submissions.create({
-						owner: request.user.id,
-						media: submitMedia.id,
-						thumbnail: thumbnailMedia && thumbnailMedia.id,
-						title: form.fields.title,
-						description: form.fields.description,
-						rating: form.fields.rating,
-						tags: form.fields.tags
-					});
-				}).then(function (submissionId) {
-					var titleSlug = slug.slugFor(form.fields.title);
-					var submissionPath = util.format('/submissions/%d/%s', submissionId, titleSlug);
-
-					return new Redirect(submissionPath);
+	return formSession.formFiles({ thumbnail: {} }, request).then(function (form) {
+		return media.byHash(form.fields.media).then(function (submitMedia) {
+			return form.files.thumbnail.then(function (thumbnailMedia) {
+				return submissions.create({
+					owner: request.user.id,
+					media: submitMedia.id,
+					thumbnail: thumbnailMedia && thumbnailMedia.id,
+					title: form.fields.title,
+					description: form.fields.description,
+					rating: form.fields.rating,
+					tags: form.fields.tags
 				});
-			});
-		}
+			}).then(function (submissionId) {
+				var titleSlug = slug.slugFor(form.fields.title);
+				var submissionPath = util.format('/submissions/%d/%s', submissionId, titleSlug);
 
-		form.on('file', function processFile(name, file) {
-			if (name !== 'thumbnail') {
-				file.stream.resume();
-				return;
-			}
-
-			form.removeListener('file', processFile);
-
-			thumbnailUpload = media.upload(file.stream);
-		});
-
-		return new Promise(function (resolve) {
-			form.on('finish', function () {
-				resolve(completeSubmission());
+				return new Redirect(submissionPath);
 			});
 		});
 	});

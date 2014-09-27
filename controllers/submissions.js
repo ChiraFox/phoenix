13c97cae85c575cfa3bd337b81145543caf95a49
Charmander <~@charmander.me>
2014-09-26 20:55:17 -0700

    Yield some submission controller promises.

--- controllers/submissions.js
+++ controllers/submissions.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Promise = require('bluebird');
 var util = require('util');
 var formSession = require('../lib/form-session');
 var permission = require('../lib/permission');
@@ -14,38 +15,35 @@ var submissions = require('../models/submissions');
 var uploadForm = permission.require('submit', view('submissions/upload', [notifications.counts]));
 var createForm = permission.require('submit', view('submissions/edit', [tags.mostCommonTagsForRequester, media.forRequest, notifications.counts]));
 
-var upload = permission.require('submit', function upload(request) {
-	return formSession.formFiles({ file: {} }, request).then(function (form) {
-		return form.files.file;
-	}).then(function (submissionMedia) {
-		return submissionMedia ?
-			new Redirect('/submissions/new?submit=' + submissionMedia.hash, Redirect.SEE_OTHER) :
-			new Redirect('/submissions/upload');
-	});
-});
+var upload = permission.require('submit', Promise.coroutine(function* upload(request) {
+	var form = yield formSession.formFiles({ file: {} }, request);
+	var submissionMedia = yield form.files.file;
 
-var create = permission.require('submit', function create(request) {
-	return formSession.formFiles({ thumbnail: {} }, request).then(function (form) {
-		return media.byHash(form.fields.media).then(function (submitMedia) {
-			return form.files.thumbnail.then(function (thumbnailMedia) {
-				return submissions.create({
-					owner: request.user.id,
-					media: submitMedia.id,
-					thumbnail: thumbnailMedia && thumbnailMedia.id,
-					title: form.fields.title,
-					description: form.fields.description,
-					rating: form.fields.rating,
-					tags: form.fields.tags
-				});
-			}).then(function (submissionId) {
-				var titleSlug = slug.slugFor(form.fields.title);
-				var submissionPath = util.format('/submissions/%d/%s', submissionId, titleSlug);
+	return submissionMedia ?
+		new Redirect('/submissions/new?submit=' + submissionMedia.hash, Redirect.SEE_OTHER) :
+		new Redirect('/submissions/upload');
+}));
 
-				return new Redirect(submissionPath);
-			});
-		});
+var create = permission.require('submit', Promise.coroutine(function* create(request) {
+	var form = yield formSession.formFiles({ thumbnail: {} }, request);
+	var submitMedia = yield media.byHash(form.fields.media);
+	var thumbnailMedia = yield form.files.thumbnail;
+
+	var submissionId = yield submissions.create({
+		owner: request.user.id,
+		media: submitMedia.id,
+		thumbnail: thumbnailMedia && thumbnailMedia.id,
+		title: form.fields.title,
+		description: form.fields.description,
+		rating: form.fields.rating,
+		tags: form.fields.tags
 	});
-});
+
+	var titleSlug = slug.slugFor(form.fields.title);
+	var submissionPath = util.format('/submissions/%d/%s', submissionId, titleSlug);
+
+	return new Redirect(submissionPath);
+}));
 
 exports.uploadForm = uploadForm;
 exports.createForm = createForm;
